9214e908fa350d37a692b2d7297cd975560b7f7f
user <swapnilzarbade@gmail.com>
2016-11-15 18:41:11 +0530

    This is One file added.

---
+++ One.java
@@ -0,0 +1,10 @@
+public class One{
+
+	public static void main(String [] args){
+                    System.out.println("This is one file");
+ 		for (int i=1;i<=10;i++){
+                    System.out.println(i);
+                 }
+	}
+}
+            
